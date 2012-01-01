d255b9231d874e2bb014295f73e219162d705168
strix <denis.kalinkin@gmail.com>
2012-01-02 01:36:50 +0200

    added  user class and registration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetwork::Application.config.secret_token = '99fbdf70bcbf37cbe6310fde8a1b92d0c7017881523d89b32afd0fb1f9d1da5633c63aff44cfad188276f7f0cb53afb5c7255cd7333d19882292f6dc93669200'
