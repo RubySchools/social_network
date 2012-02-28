6fe2b36b4f722e0b726c37cd3aca945ecda1c617
strix <denis.kalinkin@gmail.com>
2012-02-28 16:52:59 +0200

    added download link to file in view

--- app/models/uploads.rb
+++
@@ -1,3 +0,0 @@
-class Uploads < ActiveRecord::Base
-        mount_uploader :file, FileUploader
-end
--- app/models/users_uploads.rb
+++
@@ -1,8 +0,0 @@
-class UsersUploads < ActiveRecord::Base
-#Relations
-#	belongs_to :user
-#	belongs_to :upload
-#Validations
-
-
-end
